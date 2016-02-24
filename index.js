ee782e28d04702be9e0eaa11f9744bdab58c83a7
Dan Thareja <danthareja@gmail.com>
2016-02-24 12:52:51 -0800

    Update config for eslint v2.0

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ module.exports = {
   env: {
     'es6': true
   },
-  ecmaFeatures: {
-    'jsx': true
+  parserOptions: {
+    ecmaFeatures: {
+      'jsx': true
+    }
   },
   rules: {
     /* Indentation */
@@ -32,8 +34,7 @@ module.exports = {
     'comma-spacing': 1,
     'no-multi-spaces': 1,
     'space-before-blocks': 1,
-    'space-after-keywords': 1,
-    'space-before-keywords': 1,
+    'keyword-spacing': [1, { 'before': true, 'after': true }],
     'space-infix-ops': 1,
     /* Variable declaration */
     'one-var': [1, 'never'],
